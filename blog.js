20fcbaa766dc6bd01327dfd4514974feadf7e079
Sameshnee21 <2490510@students.wits.ac.za>
2023-06-19 22:07:46 +0200

    Back to top button (blog home)

--- blog.js
+++ blog.js
@@ -1,9 +1,28 @@
-const toTop = document.querySelector(".to-top");
+/*const myBtn = document.querySelector("myBtn");
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
-        toTop.classList.add("active");
+        myBtn.classList.add("active");
     } else{
-        toTop.classList.remove("active");
+        myBtn.classList.remove("active");
     }
+})*/
+
+myButton = document.getElementById('myBtn'); 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+}
+else {
+    myButton.style.display = "none"; 
+}
+}
+
+myButton.addEventListener('click', () => {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
 })
+
